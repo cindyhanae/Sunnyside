ddf84145e7680969e9e6bba6a852dc6b29afa387
cindyhanae <cinde01@gmail.com>
2021-07-16 16:01:16 -0300

    update responsive

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function toggle(){
+    const burger = document.querySelector('#burger').classList.toggle('active');
+    const nav = document.querySelector('.nav-burger').classList.toggle('active');
+}
