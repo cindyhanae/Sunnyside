1b91ad2436c3edd3a8fecb25b5ac00f9383a001c
cindyhanae <cinde01@gmail.com>
2021-10-18 19:37:00 -0300

    event listener

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-function toggle(){
-    const burger = document.querySelector('#burger').classList.toggle('active');
-    const nav = document.querySelector('.nav-burger').classList.toggle('active');
+const burgerBtn = document.querySelector('.js-burger')
+const nav = document.querySelector('.js-nav-burger')
+
+function openMenu(){
+    nav.classList.toggle('active')
 }
 
-const menu = document.querySelector('.js-nav-burger')
-
-// menu.addEventListener('click')
+burgerBtn.addEventListener('click', openMenu)
