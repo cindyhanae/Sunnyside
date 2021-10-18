6f1ac349b48bdee78298cdb2ebd10c0eea9e0dad
cindyhanae <cinde01@gmail.com>
2021-10-18 19:25:45 -0300

    dropdown

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ function toggle(){
     const burger = document.querySelector('#burger').classList.toggle('active');
     const nav = document.querySelector('.nav-burger').classList.toggle('active');
 }
+
+const menu = document.querySelector('.js-nav-burger')
+
+// menu.addEventListener('click')
