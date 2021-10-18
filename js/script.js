6e42459fc87f11dfb76eb462fc3b0b8e56716c76
cindyhanae <cinde01@gmail.com>
2021-10-18 20:31:22 -0300

    update mobile menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
 const burgerBtn = document.querySelector('.js-burger')
 const nav = document.querySelector('.js-nav-burger')
+const navItem = document.querySelectorAll('.js-nav-burger ul li a')
+const body = document.querySelector('body')
 
+// open mobile menu
 function openMenu(){
     nav.classList.toggle('active')
+
+    if(nav.classList.contains('active')){
+        body.style.overflow = 'hidden'
+
+    } else if(!nav.classList.contains('active')){
+        body.style.overflow = 'auto'
+    }
+}
+
+// close menu when link is clicked
+function clickNavItem(event){
+    event.preventDefault()
+    nav.classList.remove('active')
+
+    if(!nav.classList.contains('active')){
+        body.style.overflow = 'auto'
+    }
 }
 
 burgerBtn.addEventListener('click', openMenu)
+navItem.forEach(function(item){
+    item.addEventListener('click', clickNavItem)
+})
+
+
+
+
+
